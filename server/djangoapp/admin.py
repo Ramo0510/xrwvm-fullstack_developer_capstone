0c6f544e5b45bbd9ebbd173827d9e8fdc0017b2b
Ramo0510 <omardij01@gmail.com>
2024-07-27 12:57:15 -0400

    code updated

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -4,9 +4,3 @@ from .models import CarMake, CarModel
 # Register your models here.
 admin.site.register(CarMake)
 admin.site.register(CarModel)
-
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
